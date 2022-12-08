be2dd721deccb4940a07c87dd51668eb8beac217
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-12-08 15:20:25 +0700

    Commit 7
    Add Task_5
    create first block for fuel

---
+++ Task_5/Task_5/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Task_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxFuelPrice = new System.Windows.Forms.TextBox();
+            this.comboBoxFuel = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxFuelPrice);
+            this.groupBox1.Controls.Add(this.comboBoxFuel);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(439, 151);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Автозаправка";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Location = new System.Drawing.Point(236, 37);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(183, 97);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Итого к оплате";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(133, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "руб.";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(23, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 43);
+            this.label6.TabIndex = 0;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(166, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "л.";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(82, 111);
+            this.maskedTextBox1.Mask = "00.00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(78, 23);
+            this.maskedTextBox1.TabIndex = 7;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(28, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Объем";
+            // 
+            // textBoxFuelPrice
+            // 
+            this.textBoxFuelPrice.Location = new System.Drawing.Point(82, 76);
+            this.textBoxFuelPrice.Name = "textBoxFuelPrice";
+            this.textBoxFuelPrice.ReadOnly = true;
+            this.textBoxFuelPrice.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxFuelPrice.Size = new System.Drawing.Size(78, 23);
+            this.textBoxFuelPrice.TabIndex = 3;
+            // 
+            // comboBoxFuel
+            // 
+            this.comboBoxFuel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFuel.FormattingEnabled = true;
+            this.comboBoxFuel.Location = new System.Drawing.Point(82, 37);
+            this.comboBoxFuel.Name = "comboBoxFuel";
+            this.comboBoxFuel.Size = new System.Drawing.Size(111, 23);
+            this.comboBoxFuel.TabIndex = 0;
+            this.comboBoxFuel.SelectedIndexChanged += new System.EventHandler(this.comboBoxFuel_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "руб.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(28, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Цена";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(17, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Топливо";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 169);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(439, 145);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Мини-кафе";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(12, 320);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(439, 118);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Итого к оплате";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private Label label1;
+        private ComboBox comboBoxFuel;
+        private TextBox textBoxFuelPrice;
+        private Label label2;
+        private Label label3;
+        private GroupBox groupBox4;
+        private Label label4;
+        private Label label5;
+        private MaskedTextBox maskedTextBox1;
+        private Label label7;
+        private Label label6;
+    }
+}
---
+++ Task_5/Task_5/Form1.cs
@@ -0,0 +1,67 @@
+﻿namespace Task_5
+{
+    public partial class Form1 : Form
+    {
+        double[] fuel_price = { 42.30, 48.55, 46.15 };
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxFuel.Items.AddRange(new string[] { "Бензин АИ92", "Бензин АИ95", "Дизель" });
+            maskedTextBox1.TextChanged += MaskedTextBox1_TextChanged;
+
+            
+        }
+
+        private void MaskedTextBox1_TextChanged(object? sender, EventArgs e)
+        {
+            switch (comboBoxFuel.SelectedIndex)
+            {
+                case 0:
+                    {
+                        double sum = Convert.ToDouble(maskedTextBox1.Text) * 42.30;
+                        label6.Text = sum.ToString();
+                        break;
+                    }
+                case 1:
+                    {
+                        double sum = Convert.ToDouble(maskedTextBox1.Text) * 48.55;
+                        label6.Text = sum.ToString();
+                        break;
+                    }
+                case 2:
+                    {
+                        double sum = Convert.ToDouble(maskedTextBox1.Text) * 46.15;
+                        label6.Text = sum.ToString();
+                        break;
+                    }
+            }
+        }
+
+        private void comboBoxFuel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxFuel.SelectedIndex)
+            {
+                case 0:
+                    {
+                        textBoxFuelPrice.Text = fuel_price[0].ToString();
+                        maskedTextBox1.Clear();
+                        break;
+                    }
+                case 1:
+                    {
+                        textBoxFuelPrice.Text = fuel_price[1].ToString();
+                        maskedTextBox1.Clear();
+                        break;
+                    }
+                case 2:
+                    {
+                        textBoxFuelPrice.Text = fuel_price[2].ToString();
+                        maskedTextBox1.Clear();
+                        break;
+                    }
+            }
+        }
+       
+        
+    }
+}
