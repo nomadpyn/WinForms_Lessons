30175bf1238674fb1f0dc7eec974cb1cac899756
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-12-12 17:08:07 +0700

    Commit 20
    Task_7
    Create class Student

---
+++ Task_7/Task_7/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    public class Student
+    {
+        public string? Name { get; set; }
+        public string? Fname { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string? Group { get; set; }
+        public Student(string? name, string? fname, DateTime birthDay, string? group)
+        {
+            this.Name = name;
+            this.Fname = fname;
+            this.BirthDay = birthDay;
+            this.Group = group;
+        }
+        public override string ToString()
+        {
+            return $"{this.Fname} {this.Name}. Дата рождения {this.BirthDay.Date}. Группа {this.Group}."; 
+        }
+    }
+}
