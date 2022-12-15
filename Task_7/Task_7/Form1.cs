a8af5d63299ff0da779b47836c9523dc3b943293
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-12-15 14:50:13 +0700

    Commit 26
    Task_7
    Add comments

--- Task_7/Task_7/Form1.cs
+++ Task_7/Task_7/Form1.cs
@@ -1,4 +1,4 @@
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 using System;
 using System.Xml.Linq;
 using System.Windows.Forms;
@@ -11,19 +11,19 @@ namespace Task_7
         public Form1()
         {
             InitializeComponent();
+// Выбор поля Fname из класса Student для отображения в ListBox
             listBoxStudents.DisplayMember = "Fname";
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             listBoxStudents.MouseDoubleClick += ListBoxStudents_MouseDoubleClick;
         }
-
+// Открытие информации о студенте по двойному клику в поле ListBox
         private void ListBoxStudents_MouseDoubleClick(object? sender, MouseEventArgs e)
         {
             MessageBox.Show(listBoxStudents.SelectedItem.ToString());
         }
-
+// открытие формы для заполнения данных о студенте и последующее сохранения данных в ListBox
         private void buttonAdd_Click(object sender, EventArgs e)
         {
             Form2 AddForm = new Form2();
@@ -31,50 +31,60 @@ namespace Task_7
             {
                 listBoxStudents.Items.Add(AddForm.ST);
             }
-            this.checkButtons();
-            
+            this.checkButtons();  
         }
-
+// обработка действия при нажатии кнопки Сохранить
         private void buttonSave_Click(object sender, EventArgs e)
         {
+// Создание диалога для сохранения файла в формате xml
             SaveFileDialog sfd = new SaveFileDialog();
+// Создание фильтра для файлов в диалоговом окне
             sfd.Filter = "XML Files (*.xml)|*.xml";
             sfd.FilterIndex = 0;
+// Выбор разрешения файла по умолчанию
             sfd.DefaultExt = "xml";
+// При успешном открытии диалога продолжение работы программы
             if (sfd.ShowDialog() == DialogResult.OK)
             {
+// Получаем путь файла, в который сохраняем
                 string filename = sfd.FileName;
+// Создаем List студентов и забираем в него данные из ListBox
                 List<Student> students = new List<Student>();
                 foreach (var s in listBoxStudents.Items)
                 {
                     students.Add((Student)s);
                 }
-
+// Создаем Xml Сериализатор для сохранения List в файл
                 XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Student>));
-
                 using (Stream fStream = File.Create(filename))
                 {
                     xmlFormat.Serialize(fStream, students);
                 }
             }
         }
-
+// обработка действия при нажатии кнопки Загрузить
         private void buttonLoad_Click(object sender, EventArgs e)
         {
+// Создание диалога для открытия файла в формате xml
             OpenFileDialog ofd = new OpenFileDialog();
+ // Создание фильтра для файлов в диалоговом окне
             ofd.Filter = "XML Files (*.xml)|*.xml";
             ofd.FilterIndex = 0;
+// Выбор разрешения файла по умолчанию
             ofd.DefaultExt = "xml";
+// При успешном открытии диалога продолжение работы программы
             if (ofd.ShowDialog() == DialogResult.OK)
             {
+// Получаем путь файла, из которого загружаем
                 string filename = ofd.FileName;
-
+// Создаем Xml Сериализатор для записи данных из файла в List
                 XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Student>));
                 List<Student> students = null;
                 using (Stream fStream = File.OpenRead(filename))
                 {
                     students = (List<Student>)xmlFormat.Deserialize(fStream);
                 }
+// Перенос данных из List в ListBox для отображения
                 foreach (var s in students)
                 {
                     listBoxStudents.Items.Add(s);
@@ -82,6 +92,7 @@ namespace Task_7
             }
             this.checkButtons();
         }
+// В случае если есть данные в ListBox, кнопка Сохранить активна, а кнопка Загрузить не активна
         private void checkButtons()
         {
             if (listBoxStudents.Items.Count > 0)
--- Task_7/Task_7/Form2.cs
+++ Task_7/Task_7/Form2.cs
@@ -12,39 +12,39 @@ namespace Task_7
 {
     public partial class Form2 : Form
     {
+// Создаем "пустого" Студента для добавления в него данных из полей
         public Student ST =new Student();
         public Form2()
         {
             InitializeComponent();
+// Создаем список данных для отображения групп в ComboBox
             string[] groups = { "СТ3", "БВ1", "ВК5" };
             comboBoxGroup.Items.AddRange(groups);                       
         }
-
+// Записываем фамилию из поля textBoxFname в поле студента
         private void textBoxFname_TextChanged(object sender, EventArgs e)
         {
             this.ST.Fname = textBoxFname.Text;
         }
-
+// Записываем имя из поля textBoxName в поле студента
         private void textBoxName_TextChanged(object sender, EventArgs e)
         {
             this.ST.Name = textBoxName.Text;
         }
-
+// Записываем группу в поле студента при выборе ее в ComboBox
         private void comboBoxGroup_SelectedIndexChanged(object sender, EventArgs e)
         {
             this.ST.Group = comboBoxGroup.Text;
         }
-
+// Забираем дату в поле студента из dateTimePicker
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
-            //int day = dateTimePicker1.Value.Day;
-            //int month = dateTimePicker1.Value.Month;
-            //int year = dateTimePicker1.Value.Year;
             this.ST.BirthDay = dateTimePicker1.Value;
+// если все поля имеют какое-то значение, кнопка Добавить становиться активной
             if (ST.Name != null && ST.Fname != null && ST.Group != null)
                 buttonAddToList.Enabled = true;
         }
-
+// Возвращаем результат диалога и объект в первую форму
         private void buttonAddToList_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.OK;
